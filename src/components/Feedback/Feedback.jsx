8a808ff3afa0de375f29a85047cf7cb9cf5110d4
Casimova Marina <maracasimova@gmail.com>
2023-05-08 21:02:23 +0300

    Step 2

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -14,6 +14,17 @@ export const Feedback = () => {
     }));
   };
 
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = state;
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = state;
+    const total = countTotalFeedback();
+    return total > 0 ? Math.round((good / total) * 100) : 0;
+  };
+
   return (
     <div>
       <h1>Please leave feedback</h1>
@@ -21,9 +32,17 @@ export const Feedback = () => {
       <button onClick={() => handleButtonClick('neutral')}>Neutral</button>
       <button onClick={() => handleButtonClick('bad')}>Bad</button>
       <h2>Statistics</h2>
-      <p>Good: {state.good}</p>
-      <p>Neutral: {state.neutral}</p>
-      <p>Bad: {state.bad}</p>
+      {countTotalFeedback() === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <div>
+          <p>Good: {state.good}</p>
+          <p>Neutral: {state.neutral}</p>
+          <p>Bad: {state.bad}</p>
+          <p>Total: {countTotalFeedback()}</p>
+          <p>Positive feedback: {countPositiveFeedbackPercentage()}%</p>
+        </div>
+      )}
     </div>
   );
 };
