773de9f27fd74f71ef83d241cb52be064f0a7d84
Casimova Marina <maracasimova@gmail.com>
2023-05-08 21:45:49 +0300

    Step 3 Section and Statistics

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import style from './Statistics.module.css';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <div>
+      <p className={style.title}>Good: {good}</p>
+      <p className={style.title}>Neutral: {neutral}</p>
+      <p className={style.title}>Bad: {bad}</p>
+      <p className={style.title}>Total: {total}</p>
+      <p className={style.title}>Positive feedback: {positivePercentage}%</p>
+    </div>
+  );
+};
+
+Statistics.propTypes = {
+  positivePercentage: PropTypes.func.isRequired,
+  total: PropTypes.func.isRequired,
+};
